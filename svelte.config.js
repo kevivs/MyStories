a091c5eb5f0a2a163f124e67103b48cc9b6a0e72
kevivs <kevivs@gmail.com>
2025-07-27 10:53:14 +0530

    Deploy run 1

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,20 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() ,
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
     paths: {
       base: '/MyStories' // Adjust this if deploying to a subfolder
-    }}
+    }
+	}
 };
 
 export default config;
