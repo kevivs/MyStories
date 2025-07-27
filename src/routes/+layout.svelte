e6356c2c62cfdd9fe167a9daa64e3abf26dbd724
kevivs <kevivs@gmail.com>
2025-07-27 11:39:19 +0530

    deploy run 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,6 @@
 <script lang="ts">
   import '@fontsource/crimson-pro';
+  import '../app.css';
 </script>
 
 <slot />
-
-<style>
-  :global(body) {
-    background-color: #111;
-    color: #e0e0e0;
-    font-family: 'Crimson Pro', serif;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-  }
-</style>
