ec08c7a345db56dfec77c50799adc0917a8cbbbe
kevivs <kevivs@gmail.com>
2025-07-27 11:59:16 +0530

    deploy run 6

--- svelte.config.js
+++ svelte.config.js
@@ -17,8 +17,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/MyStories' : '',
-			assets: process.env.NODE_ENV === 'production' ? '/MyStories' : ''
+			base: process.env.NODE_ENV === 'production' ? '/MyStories' : ''
 		},
 		prerender: {
 			handleHttpError: ({ path, referrer, message }) => {
